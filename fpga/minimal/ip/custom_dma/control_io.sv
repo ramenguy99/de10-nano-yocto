61db66f46c0ded94183bcada1509ba18cbc2157c
Dario Mylonopoulos <dmylos@yahoo.it>
2026-04-19 22:55:12 +0200

    fpga: custom dma avalon mm testing

--- fpga/minimal/ip/custom_dma/control_io.sv
+++ fpga/minimal/ip/custom_dma/control_io.sv
@@ -6,6 +6,7 @@ module control_io (
   //
   // Control IO
   input logic avs_s0_write,
+  input logic [3:0] avs_s0_address,
   input logic [31:0] avs_s0_writedata,
   input logic avs_s0_read,
   output logic [31:0] avs_s0_readdata,
@@ -24,21 +25,111 @@ module control_io (
   output  logic [10:0]  avm_m0_burstcount,
 
   // Interrupts
-  output logic interrupt_sender_irq
+  output logic interrupt_sender_irq,
+
+  // Debug output
+  output logic [255:0] out_data
 );
 
+// Control IO
+logic trigger;
+logic [31:0] sdram_read_addr;
+logic [31:0] sdram_write_addr;
+
+always_ff @(posedge clk ) begin
+  trigger <= 0;
+
+  if (avs_s0_write)
+  begin
+    case (avs_s0_address)
+      4'b0000: trigger <= avs_s0_writedata[0];
+      4'b0001: sdram_read_addr <= avs_s0_writedata;
+      4'b0010: sdram_write_addr <= avs_s0_writedata;
+      default: begin end
+    endcase
+  end
+
+  if (reset) trigger <= 0;
+end
+
+
+// State machine
+localparam INIT = 2'd0;
+localparam READ_START = 2'd1;
+localparam READ_END = 2'd2;
+
+logic [1:0] cur_state;
+logic [1:0] next_state;
+
+
 always_comb begin
-  interrupt_sender_irq = 1'b0;
-  avm_m0_write = 1'b1;
+  next_state = cur_state;
+  case(cur_state)
+    INIT: begin
+      if (trigger) next_state = READ_START;
+    end
+
+    READ_START: begin
+      if (avm_m0_waitrequest) next_state = READ_START; // Wait here.
+      else next_state = READ_END;
+    end
+
+    READ_END: begin
+      if (!avm_m0_readdatavalid) next_state = READ_END; // Wait here.
+      else next_state = INIT;
+    end
+
+    default: begin
+      next_state = INIT;
+    end
+  endcase
 end
 
 always_ff @(posedge clk ) begin
-  avm_m0_read = ~avm_m0_read;
-  // avm_m0_read = ~avm_m0_read;
+  if (reset) cur_state <= INIT;
+  else cur_state <= next_state;
+end
 
-  if (reset) begin
-      avm_m0_read = 1'b0;
+// Avalon master read
+always_comb begin
+  avm_m0_address = 32'd0;
+  avm_m0_read = 1'b0;
+  avm_m0_byteenable = 32'd0;
+  avm_m0_burstcount = 11'd0;
+
+  case(cur_state)
+
+    READ_START: begin
+      avm_m0_address = sdram_read_addr;
+      avm_m0_read = 1'b1;
+      avm_m0_byteenable = 32'h0000_000F; // Get 32 bits only.
+      avm_m0_burstcount = 11'd1; // Get only 1 address value.
+    end
+
+    default: begin
+    end
+  endcase
+end
+
+always_ff @(posedge clk) begin
+  if (reset) out_data <= 256'd0;
+  else begin
+    case (cur_state)
+      READ_END: begin
+        if (avm_m0_readdatavalid) begin
+          out_data <= avm_m0_readdata;
+        end
+      end
+
+      default: begin
+      end
+    endcase
   end
+end
+
+// Interrupts
+always_ff @(posedge clk) begin
+  if (reset) interrupt_sender_irq <= 1'b0;
 end
 
 endmodule
--- fpga/minimal/ip/custom_dma/tb/test_control_io.py
+++ fpga/minimal/ip/custom_dma/tb/test_control_io.py
@@ -3,7 +3,6 @@ from cocotb.triggers import Timer, RisingEdge, FallingEdge
 from cocotb.clock import Clock
 from cocotb.handle import Immediate
 
-
 async def cycle_reset(dut):
     dut.reset.value = 0
     await RisingEdge(dut.clk)
@@ -15,18 +14,59 @@ async def cycle_reset(dut):
     await RisingEdge(dut.clk)
 
 @cocotb.test()
-async def my_first_test(dut):
+async def custom_dma_test(dut):
     # Clock
     cocotb.start_soon(Clock(dut.clk, 1, unit="ns").start())
+
+    TEST_VALUE = 0x1234
+    READ_ADDRESS = 0xdeadbeef
+    WRITE_ADDRESS = 0xcafebabe
+
+    # Init
+    dut.avm_m0_readdata.value = TEST_VALUE
+    dut.avm_m0_readdatavalid.value = 0
 
     # Reset
     await cycle_reset(dut)
 
-    # Run some clocks
-    CLOCKS = 6
-    await Timer(CLOCKS, unit="ns")
+    dut.avm_m0_waitrequest.value = 1
 
-    # cocotb.log.info("my_signal_1 is %s", dut.my_signal_1.value)
+    # Write read addr
+    dut.avs_s0_address.value = 1
+    dut.avs_s0_write.value = 1
+    dut.avs_s0_writedata.value = READ_ADDRESS
+    await RisingEdge(dut.clk)
+
+    # Write write addr
+    dut.avs_s0_address.value = 2
+    dut.avs_s0_write.value = 1
+    dut.avs_s0_writedata.value = WRITE_ADDRESS
+    await RisingEdge(dut.clk)
+
+    # Write trigger
+    dut.avs_s0_address.value = 0
+    dut.avs_s0_write.value = 1
+    dut.avs_s0_writedata.value = 1
+    await RisingEdge(dut.clk)
+
+    # Reset write signals
+    dut.avs_s0_address.value = 0
+    dut.avs_s0_write.value = 0
+    dut.avs_s0_writedata.value = 0
+
+    # Simulate bus busy before taking in read
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.avm_m0_waitrequest.value = 0
+    assert dut.avm_m0_address.value == READ_ADDRESS
+
+    # Simulate read takes 2 cycles
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+    dut.avm_m0_readdatavalid.value = 1
+
+    # Drain
+    await Timer(10, unit="ns")
+
     assert dut.interrupt_sender_irq.value == 0
-    assert dut.avm_m0_write.value == 1
-    assert dut.avm_m0_read.value == CLOCKS % 2
+    assert dut.out_data.value == TEST_VALUE
